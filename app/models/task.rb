7ab29bf2a2bca23df688402a63e7429329c6bd2b
derrekrb <derrek.ray@gmail.comcom>
2015-10-10 10:18:18 -0600

    set up paperclip

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,4 @@
 class Task < ActiveRecord::Base
+  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ""
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
