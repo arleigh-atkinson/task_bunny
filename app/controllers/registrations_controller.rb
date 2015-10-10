0087f27a9528342937b24bf0fd48e523b61039c6
Will <will@thefixitgeek.com>
2015-10-10 11:02:19 -0600

    update views fix typos

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,6 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   private
     def sign_up_params
-      params.require(:user).permite(:first_name, :last_name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
   end
